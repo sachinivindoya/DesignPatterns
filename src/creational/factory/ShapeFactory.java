0d95ef2be54592df2b759486a2148f41fd46551b
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-11 10:12:01 +0530

    factory design pattern

--- src/creational/factory/ShapeFactory.java
+++ src/creational/factory/ShapeFactory.java
@@ -10,11 +10,11 @@ public class ShapeFactory {
         if(shapeType==null){
             return null;
         }else{
-            if( shapeType.equalsIgnoreCase("Circle")){
+            if( shapeType.equalsIgnoreCase("CIRCLE")){
                 return new Circle();
-            }else if (shapeType.equalsIgnoreCase("Rectangle")){
+            }else if (shapeType.equalsIgnoreCase("RECTANGLE")){
                 return new Rectangle();
-            }else if(shapeType.equalsIgnoreCase("square")){
+            }else if(shapeType.equalsIgnoreCase("SQUARE")){
                 return new Square();
             }
         }
